f4724e95485246dbfa3cca315d6b6678be2e1631
elvisbkt <47518658+elvisbkt@users.noreply.github.com>
2019-08-13 00:11:20 +0300

    requests file

---
+++ app/requests.py
@@ -0,0 +1,15 @@
+import urllib.request,json
+from .models import Quote
+
+
+
+def get_quote():
+
+    with urllib.request.urlopen('http://quotes.stormconsultancy.co.uk/random.json') as url:
+        quote_details_data = url.read()
+        quote_details_response = json.loads(quote_details_data)
+
+        
+
+    return quote_object
+
