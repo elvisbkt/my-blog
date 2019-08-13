178fb9cfe42c2ced1179f4affe38ecd9ceb0c492
elvisbkt <47518658+elvisbkt@users.noreply.github.com>
2019-08-13 10:33:51 +0300

    configure start module

---
+++ start.sh
@@ -0,0 +1,6 @@
+export DATABASE_URL='postgresql+psycopg2://postgres:7777@localhost/blogs'
+export SECRET_KEY='7777'
+export MAIL_USERNAME='elvisbkt@gmail.com'
+export MAIL_PASSWORD='raphnado'
+
+python manage.py server
